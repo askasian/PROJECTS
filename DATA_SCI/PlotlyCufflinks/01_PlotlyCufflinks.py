ac651eaebc654e1c4a443ba70ceb3df0319f16e1
askasian <askasyan@gmail.com>
2021-03-16 13:44:30 +0200

    'PlotlyChufflinks done'

--- DATA_SCI/PlotlyCufflinks/01_PlotlyCufflinks.py
+++ DATA_SCI/PlotlyCufflinks/01_PlotlyCufflinks.py
@@ -36,3 +36,20 @@ df.sum().iplot(kind='bar')
 # %%
 df.iplot(kind='box')
 # %%
+df3 = pd.DataFrame({'x': [1, 2, 3, 4, 5], 'y': [
+                   10, 20, 30, 40, 50], 'z': [5, 4, 3, 2, 1]})
+df3
+
+# %%
+df3.iplot(kind='surface')
+df3.iplot(kind='surface', colorscale='rdylbu')
+# %%
+df['A'].iplot(kind='hist', bins=25)
+df.iplot(kind='hist')
+# %%
+df[['A', 'B']].iplot(kind='spread')
+# %%
+df.iplot(kind='bubble', x='A', y='B', size='C')
+# %%
+df.scatter_matrix()
+# %%
