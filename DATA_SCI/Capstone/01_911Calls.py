cdcd3322b71bb320759dce1063441aef82bacd57
askasian <askasyan@gmail.com>
2021-03-22 10:48:32 +0200

    '911 start'

---
+++ DATA_SCI/Capstone/01_911Calls.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('/run/media/wiskey/EX/DATA_FILES/911.csv')
+
+print(df.info())
+print(df.head())
