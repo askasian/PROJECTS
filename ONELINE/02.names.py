65af75818e41f087f8d507f34d58cd9416f8bd0b
askasian <askasyan@gmail.com>
2021-07-29 12:53:12 +0300

    names progress

--- ONELINE/02.names.py
+++ ONELINE/02.names.py
@@ -7,3 +7,10 @@ for _ in range(25):
     names.append(faker.first_name())
 
 print(names)
+
+name_starts_a = [name for name in names if name.startswith("A")]
+print(name_starts_a)
+
+name_starts_a = filter(lambda name: name.startswith("A"), names)
+print(list(name_starts_a))
+print(tuple(name_starts_a))
