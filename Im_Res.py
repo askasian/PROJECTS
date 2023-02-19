bccbe30469b4d3e01819fb7248e3d1c8c542013c
askasian <askasyan@gmail.com>
2023-02-19 11:48:54 +0200

    imres changed

--- Im_Res.py
+++ Im_Res.py
@@ -8,4 +8,4 @@ for path, currentDirectory, files in os.walk("/usr/share/backgrounds"):
         input_image = Image.open(os.path.join(path, file))
 
         output = input_image.resize((1920, 1080))
-        output.save(os.path.join(path, file) + "_resized")
+        output.save(os.path.join(path, file) + "_resized.png")
